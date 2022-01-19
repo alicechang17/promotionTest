9e34b938849a65f749848ec8fa3ab7c90eea2d5f
Alice <alicechang@17.media>
2022-01-19 22:36:14 +0800

    Initial Commit

---
+++ promotionArea/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  promotionArea
+//
+//  Created by Alice Chang on 2022/1/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
